4ca9e9adcecfccfffad39f87b50128e6590a4451
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-20 20:23:23 -0300

    se creo el archivo numMayor en el cual se pide al usuario que ingrese 2 numeros y se muestra en pantalla cual es el número mayor

---
+++ js/condicionales/6-numMayor.js
@@ -0,0 +1,9 @@
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+
+if(num1 > num2){
+    document.write("<h2>El número mayor es: " + num1 + "</h2>");
+}
+else{
+    document.write("<h2>El número mayor es: " + num2 + "</h2>");
+}
