851cc22712375edf24e724f33e6e00e0e5f574c5
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-20 13:34:50 -0300

    se realizo la conversion de grados celsius a fahrenheit mediante una formula y se mostro por consola

---
+++ js/variables/3-conversion.js
@@ -0,0 +1,5 @@
+let gradosCelsius = 35;
+
+let gradosFahrenheit = (gradosCelsius * (9/5)) + 32;
+
+console.log("La conversión de " + gradosCelsius + " °C en Fahrenheit es: " + gradosFahrenheit + " °F");
