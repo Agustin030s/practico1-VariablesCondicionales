cf1271f4c38b330f0dc55d0debeceaece2b980e4
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-20 20:48:44 -0300

    se creo el archivo divisibilidad2 el cual le pide un numero al usuario y devuelve el primer número por el cual es divisible

---
+++ js/condicionales/10-divisibilidad2.js
@@ -0,0 +1,15 @@
+let num1 = parseInt(prompt("Ingrese un número"));
+
+if(num1 % 2 === 0){
+   document.write("<h2>El número " + num1 + " es divisible por 2</h2>");
+} 
+else if(num1 % 3 === 0){
+   document.write("<h2>El número " + num1 + " es divisible por 3</h2>");
+} 
+else if(num1 % 5 === 0){
+   document.write("<h2>El número " + num1 + " es divisible por 5</h2>");
+} 
+else if(num1 % 7 === 0){
+   document.write("<h2>El número " + num1 + " es divisible por 7</h2>");
+} 
+
---
+++ js/condicionales/8-divisibilidad.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("Ingrese un número"));
+
+if(num1 % 2 === 0){
+    document.write("<h2>El número " + num1 + " es divisible por 2</h2>");
+} 
+else {
+    document.write("<h2>El número " + num1 + " no es divisible por 2</h2>");
+}
