b7e430dfc58b415ef09621b36b414646f413bbe6
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-20 20:18:15 -0300

    se creo el archivo sumaNumIngresados en el que se pide al usuario que ingrese dos numeros y se muestra por pantalla la suma de estos numeros

---
+++ js/condicionales/5-sumaNumIngresados.js
@@ -0,0 +1,3 @@
+let num1 = parseInt(prompt("Ingrese el primer número"));
+let num2 = parseInt(prompt("Ingrese el segundo número"));
+document.write("<h2> La suma de los numeros ingresados es: " + (num1 + num2) + "</h2>");
