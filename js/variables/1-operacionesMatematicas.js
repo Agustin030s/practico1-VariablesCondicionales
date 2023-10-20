01a53c7ae445b82332531df7d52204ed523491b3
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-10-20 13:25:16 -0300

    se desarrollaron las operaciones matematicas de suma, resta, multiplicación y división en el archivo operacionesMatematicas

---
+++ js/variables/1-operacionesMatematicas.js
@@ -0,0 +1,20 @@
+let numero1 = 50;
+let numero2 = 70;
+
+let resultado = null;
+
+//Suma
+resultado = numero1 + numero2;
+console.log("El resultado de la suma de " + numero1 + " + " + numero2 + " es " + resultado);
+
+//Resta
+resultado = numero1 - numero2;
+console.log("El resultado de la resta de " + numero1 + " - " + numero2 + " es " + resultado);
+
+//Multiplicación
+resultado = numero1 * numero2;
+console.log("El resultado de la multiplicación de " + numero1 + " * " + numero2 + " es " + resultado);
+
+//División
+resultado = numero1 / numero2;
+console.log("El resultado de la división de " + numero1 + " / " + numero2 + " es " + resultado);
